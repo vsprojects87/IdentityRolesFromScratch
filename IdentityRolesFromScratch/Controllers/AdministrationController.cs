0c2bbdf6a49970714199615801d32c0865ae7025
vsprojects87 <vs.scala.619@gmail.com>
2024-05-24 10:55:59 +0530

    Added roles and user manager

---
+++ IdentityRolesFromScratch/Controllers/AdministrationController.cs
@@ -0,0 +1,173 @@
+﻿using IdentityRolesFromScratch.Models;
+using IdentityRolesFromScratch.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityRolesFromScratch.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<AppUser> userManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<AppUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+
+		//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+		// Users
+
+        [HttpGet]
+        public IActionResult ListUsers()
+        {
+            var users = userManager.Users;
+            return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id) {
+			var user = await userManager.FindByIdAsync(id);
+			if (user == null)
+			{
+				ViewBag.ErrorMessage = $"User with Id={id} cannot be found";
+				return View("NotFound");
+			}
+
+			var userClaims = await userManager.GetClaimsAsync(user);
+			var userRoles = await userManager.GetRolesAsync(user);
+
+            var model = new EditUserVM()
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                Address = user.Address,
+                Claims = userClaims.Select(c => c.Value).ToList(),
+                // claims have both name and value thats why selecting value only
+                Roles = userRoles
+            };
+			return View(model);
+        }
+
+		[HttpPost]
+		public async Task<IActionResult> EditUser(EditUserVM model)
+		{
+			var user = await userManager.FindByIdAsync(model.Id);
+			if (user == null)
+			{
+				ViewBag.ErrorMessage = $"User with Id={model.Id} cannot be found";
+				return View("NotFound");
+			}
+			else
+			{
+				user.Email = model.Email;
+				user.UserName = model.UserName;
+				user.Address = model.Address;
+				var result = await userManager.UpdateAsync(user);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("ListUsers");
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+			}
+			return View(model);
+		}
+
+		public async Task<IActionResult> DeleteUser(string id)
+		{
+			var user = await userManager.FindByIdAsync(id);
+			if (user == null)
+			{
+				ViewBag.ErrorMessage = $"User with Id={id} cannot be found";
+				return View("NotFound");
+			}
+			else
+			{
+				var result = await userManager.DeleteAsync(user);
+
+				if (result.Succeeded)
+				{
+					return RedirectToAction("ListUsers");
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+				return View("ListUsers");
+			}
+		}
+
+
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+		// Roles
+
+		[HttpGet]
+		public IActionResult CreateRole()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateRole(CreateRoleVM model)
+		{
+			if (ModelState.IsValid)
+			{
+				IdentityRole identityRole = new IdentityRole()
+				{
+					Name = model.RoleName
+				};
+				IdentityResult result = await roleManager.CreateAsync(identityRole);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("index", "home");
+				}
+				foreach (IdentityError error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+			}
+			return View(model);
+		}
+
+		[HttpGet]
+		public IActionResult ListRoles()
+		{
+			var roles = roleManager.Roles;
+			return View(roles);
+		}
+
+		public async Task<IActionResult> DeleteRole(string id)
+		{
+			var role = await roleManager.FindByIdAsync(id);
+			if (role == null)
+			{
+				ViewBag.ErrorMessage = $"Role with Id={id} cannot be found";
+				return View("NotFound");
+			}
+			else
+			{
+				var result = await roleManager.DeleteAsync(role);
+
+				if (result.Succeeded)
+				{
+					return RedirectToAction("ListRoles");
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+				return View("ListRoles");
+			}
+		}
+
+	}
+}
---
+++ IdentityRolesFromScratch/Models/ViewModels/CreateRoleVM.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityRolesFromScratch.Models.ViewModels
+{
+	public class CreateRoleVM
+	{
+		[Required]
+		public string RoleName { get; set; }
+	}
+}
